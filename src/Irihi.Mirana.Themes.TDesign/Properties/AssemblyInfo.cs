b136f06a7ad32d491bfbe48993aa2dc11a928768
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-01-29 19:29:39 +0800

    feat: add Avatar.

--- src/Irihi.Mirana.Themes.TDesign/Properties/AssemblyInfo.cs
+++ src/Irihi.Mirana.Themes.TDesign/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@
 [assembly: XmlnsPrefix("https://irihi.tech/td", "td")]
 [assembly: XmlnsDefinition("https://irihi.tech/td", "Irihi.Mirana.Themes.TDesign")]
 [assembly: XmlnsDefinition("https://irihi.tech/td", "Irihi.Mirana.Themes.TDesign.Tokens")]
+[assembly: XmlnsDefinition("https://irihi.tech/td", "Irihi.Mirana.Themes.TDesign.Converters")]
