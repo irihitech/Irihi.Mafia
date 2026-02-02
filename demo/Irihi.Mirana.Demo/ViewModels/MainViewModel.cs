da336b8e26396d966e596b9987f5ece1634159c7
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-02-03 00:24:07 +0800

    feat: refactor MainViewModel and MainView for improved readability and structure; add Semi.Avalonia package.

--- demo/Irihi.Mirana.Demo/ViewModels/MainViewModel.cs
+++ demo/Irihi.Mirana.Demo/ViewModels/MainViewModel.cs
@@ -6,29 +6,25 @@ namespace Irihi.Mirana.Demo.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty] public partial string? Greeting { get; set; } = "Welcome to Avalonia!";
-    
-    [ObservableProperty] 
-    private PhoneResolution? _selectedResolution;
+    [ObservableProperty] public partial PhoneResolution? SelectedResolution { get; set; }
 
-    public List<PhoneResolution> PhoneResolutions { get; } = new()
-    {
-        new PhoneResolution("iPhone X", 375, 812),
-        new PhoneResolution("iPhone 16", 393, 852),
-        new PhoneResolution("iPhone 16 Pro", 402, 874),
-        new PhoneResolution("iPhone 16 Pro Max", 440, 956),
-        new PhoneResolution("Google Pixel 9", 412, 915),
-        new PhoneResolution("Google Pixel 9 Pro", 412, 915),
-        new PhoneResolution("Google Pixel 9 Pro XL", 412, 918),
-        new PhoneResolution("OPPO Find X8", 412, 918),
-        new PhoneResolution("OPPO Find X8 Pro", 450, 1000),
-        new PhoneResolution("Samsung Galaxy S24", 412, 915),
-        new PhoneResolution("Samsung Galaxy S24 Ultra", 450, 1000)
-    };
+    public List<PhoneResolution> PhoneResolutions { get; } =
+    [
+        new("iPhone X", 375, 812),
+        new("iPhone 16", 393, 852),
+        new("iPhone 16 Pro", 402, 874),
+        new("iPhone 16 Pro Max", 440, 956),
+        new("Google Pixel 9", 412, 915),
+        new("Google Pixel 9 Pro", 412, 915),
+        new("Google Pixel 9 Pro XL", 412, 918),
+        new("OPPO Find X8", 412, 918),
+        new("OPPO Find X8 Pro", 450, 1000),
+        new("Samsung Galaxy S24", 412, 915),
+        new("Samsung Galaxy S24 Ultra", 450, 1000)
+    ];
 
     public MainViewModel()
     {
-        // Set default resolution to iPhone X
         SelectedResolution = PhoneResolutions[0];
     }
 }
