506c4fc99efc0aca35ce1a23345dae86f6156404
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-02-11 00:08:56 +0800

    stash

---
+++ src/Irihi.Mirana/Common/CornerPosition.cs
@@ -0,0 +1,9 @@
+﻿namespace Irihi.Mirana.Common;
+
+public enum CornerPosition
+{
+    TopLeft,
+    TopRight,
+    BottomLeft,
+    BottomRight,
+}
--- src/Irihi.Mirana/Properties/AssemblyInfo.cs
+++ src/Irihi.Mirana/Properties/AssemblyInfo.cs
@@ -4,3 +4,4 @@
 [assembly: XmlnsDefinition("https://irihi.tech/mirana", "Irihi.Mirana")]
 [assembly: XmlnsDefinition("https://irihi.tech/mirana", "Irihi.Mirana.Common")]
 [assembly: XmlnsDefinition("https://irihi.tech/mirana", "Irihi.Mirana.Controls")]
+[assembly: XmlnsDefinition("https://irihi.tech/mirana", "Irihi.Mirana.Converters")]
