7fb6fe44cb0494e4b52371f887254b5017eab946
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-08-07 20:16:30 +0800

    fix: fix the issue of CellGroup creating nested Cells when using ItemsSource.

--- src/Irihi.Mafia/Controls/Cell/CellGroup.cs
+++ src/Irihi.Mafia/Controls/Cell/CellGroup.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
 using Avalonia.Controls.Primitives;
 
 namespace Irihi.Mafia.Controls;
@@ -8,15 +9,23 @@ public class CellGroup : HeaderedItemsControl
     protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey)
     {
         recycleKey = null;
-        return item is not ICell;
+        return item is not Control;
     }
 
-    protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey) => new Cell();
+    protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey)
+    {
+        return ItemTemplate is null ? new Cell() : new ContentPresenter();
+    }
 
     protected override void PrepareContainerForItemOverride(Control container, object? item, int index)
     {
         base.PrepareContainerForItemOverride(container, item, index);
-        if (container is Cell cell && !cell.IsSet(ContentControl.ContentTemplateProperty))
+        if (container is ContentPresenter presenter)
+        {
+            presenter.Content = item;
+            presenter.ContentTemplate = ItemTemplate;
+        }
+        else if (container is Cell cell && !cell.IsSet(ContentControl.ContentTemplateProperty))
         {
             cell.ContentTemplate = ItemTemplate;
         }
