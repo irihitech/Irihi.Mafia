9967ec75f0f73edbc74460e55c78694a377ef3e3
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-08-28 11:58:16 +0800

    chore: reference Irihi.Iconica.TDesign.

--- src/Irihi.Mafia.Themes.TDesign.Icons/Properties/Assemblyinfo.cs
+++
@@ -1,4 +0,0 @@
-using Avalonia.Metadata;
-
-[assembly: XmlnsPrefix("https://irihi.tech/td", "td")]
-[assembly: XmlnsDefinition("https://irihi.tech/td", "Irihi.Mafia.Themes.TDesign.Icons")]
--- src/Irihi.Mafia.Themes.TDesign/Icons/Core/TDesignIconFilledBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Irihi.Mafia.Themes.TDesign.Icons;
-
-public abstract class TDesignIconFilledBase : TDesignIconBase
-{
-    static TDesignIconFilledBase()
-    {
-        ModeProperty.OverrideDefaultValue<TDesignIconFilledBase>(IconMode.FilledDouble);
-    }
-}
